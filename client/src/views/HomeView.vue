3894287d0365638cbb87cecfbd98da82ea15a95e
zwl <752758299@qq.com>
2022-04-17 17:24:59 +0800

    添加前端项目

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,36 @@
+
+
+
+<template>
+  <div class="" id="app">
+    <ul>
+      <li v-for="item in data" :key="item.name">名称：{{item.name}} 电话号码：{{item.phone}}</li>
+    </ul>
+  </div>
+</template>
+
+
+
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      data: []
+    }
+  },
+  created() {
+    axios.post('/api/register?dd=222&33=67666').then(data=>{
+      this.data = data.data.data;
+    })
+  }
+}
+</script>
---
+++ client/vue.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: './',
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://127.0.0.1:8888',
+        ws: true,
+        changeOrigin: true
+      },
+    }
+  }
+})
